682c631fd689506a2081dcd252d1e5df955b9733
worldace <tokaiknight@yahoo.co.jp>
2019-11-10 15:33:54 +0900

    no message

---
+++ app/action/login_form.php
@@ -0,0 +1,22 @@
+<?php
+
+print <<<END
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex,nofollow">
+  <title>ログイン</title>
+  <link href="$blog->asset/css/login.css" rel="stylesheet">
+  <script src="$blog->asset/js/login.js" type="module"></script>
+</head>
+<body>
+
+<form action="$blog->home?action=login" method="POST">
+<input type="password" name="password"><input type="submit" value="ログイン">
+</form>
+
+
+</body>
+</html>
+END;
--- index.php
+++ index.php
@@ -7,14 +7,19 @@ include 'app/setting.php';
 
 switch(request::get('action')){
     case ''                  : $blog->go('app/action/index.php');
+    case 'index'             : $blog->go('app/action/index.php');
     case 'entry'             : $blog->go('app/action/entry.php');
     case 'entry_create'      : $blog->go('app/action/entry_create.php');
-    case 'entry_create_post' : $blog->go('app/action/entry_create_post.php');
+    case 'entry_create_form' : $blog->go('app/action/entry_create_form.php');
     case 'entry_update'      : $blog->go('app/action/entry_update.php');
-    case 'entry_update_post' : $blog->go('app/action/entry_update_post.php');
+    case 'entry_update_form' : $blog->go('app/action/entry_update_form.php');
     case 'entry_delete'      : $blog->go('app/action/entry_delete.php');
+    case 'comment'           : $blog->go('app/action/comment.php');
+    case 'comment_create'    : $blog->go('app/action/comment_create.php');
+    case 'comment_delete'    : $blog->go('app/action/comment_delete.php');
     case 'login'             : $blog->go('app/action/login.php');
-    case 'login_post'        : $blog->go('app/action/login_post.php');
+    case 'login_form'        : $blog->go('app/action/login_form.php');
     case 'logout'            : $blog->go('app/action/logout.php');
+    case 'feed'              : $blog->go('app/action/feed.php');
     default                  : $blog->go('app/action/index.php');
 }
