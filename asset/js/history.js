c323eb7ca0b0a4cd8a830b7d801c412bb157d0bd
worldace <tokaiknight@yahoo.co.jp>
2019-11-26 00:44:11 +0900

    no message

--- asset/js/history.js
+++ asset/js/history.js
@@ -3,19 +3,24 @@ const preview = document.querySelector('#history-preview').contentDocument;
 
 
 table.onclick = async function(event){
+    if(event.target.tagName !== 'TD'){
+        return;
+    }
+
     for(const tr of table.rows){
         tr.className = '';
-        if(tr.contains(event.target)){
-            tr.className = 'history-selected';
-
-            /*fetch(`?action=entry_history&history_id=${tr.dataset.id}`)
-            .then(response => response.text())
-            .then(text => preview.body.innerHTML = text);*/
-            const response = await fetch(`?action=entry_history&history_id=${tr.dataset.id}`);
-            const text     = await response.text();
-            preview.body.innerHTML = text;
-        }
     }
+    const tr = event.target.closest('tr');
+    tr.className = 'history-selected';
+
+    const response = await fetch(`?action=entry_history&history_id=${tr.dataset.id}`);
+    const text     = await response.text();
+    preview.body.innerHTML = text;
+    /*
+    fetch(`?action=entry_history&history_id=${tr.dataset.id}`)
+    .then(response => response.text())
+    .then(text => preview.body.innerHTML = text);
+    */
 };
 
 
