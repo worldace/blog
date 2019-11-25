92c2e15d707c0192e47bcd5e1a7c296759e5b6cb
worldace <tokaiknight@yahoo.co.jp>
2019-11-26 03:09:58 +0900

    no message

--- asset/js/history.js
+++ asset/js/history.js
@@ -1,13 +1,10 @@
 
-const preview = document.querySelector('#tab-content-history > iframe').contentDocument;
-
-
 document.querySelector('#history-select').onchange = function(event){
     event.stopPropagation();
 
     fetch(`?action=entry_history&history_id=${event.target.dataset.id}`)
     .then(response => response.text())
-    .then(text => preview.body.innerHTML = text);
+    .then(text => document.querySelector('#tab-content-history > iframe').contentDocument.body.innerHTML = text);
 };
 
 
@@ -15,6 +12,6 @@ document.querySelector('#history-select > button').onclick = function(event){
     if(!document.querySelector('#history-select :checked')){
         return;
     }
-    document.querySelector('textarea').value = preview.body.innerHTML;
+    document.querySelector('textarea').value = document.querySelector('#tab-content-history > iframe').contentDocument.body.innerHTML;
     document.querySelector('.tab > ul > li').click();
 };
